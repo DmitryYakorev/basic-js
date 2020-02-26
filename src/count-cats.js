38e3e71ff061e8026bc0fbf4a936db28be730ce9
urakend@mail.ru <urakend@mail.ru>
2020-02-26 22:15:24 +0300

    feat:up1

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,13 @@
-module.exports = function countCats(/* matrix */) {
+module.exports = function countCats(MD_array) {
+  let n = 0;
   throw 'Not implemented';
-  
+  for (i = 0;i < MD_array.length; i++){
+    for (let index = 0; index < MD_array[i].length; index++) {
+      if (MD_array === '^^') {
+        n++
+      }
+      
+    }
+  }
+  return n
 };
