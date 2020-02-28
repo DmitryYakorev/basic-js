187b67038007ba4e32dffe050620ccf73d6a4da2
urakend@mail.ru <urakend@mail.ru>
2020-02-28 13:18:07 +0300

    feat:task3

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,6 +2,7 @@ module.exports = function createDreamTeam(team) {
  // throw 'Not implemented';
  if (!Array.isArray(team)) return false;
  let array = [];
+ let res = '';
 
   for (i = 0; i < team.length; i++) {
     if (typeof team[i] != 'string') {
@@ -10,6 +11,10 @@ module.exports = function createDreamTeam(team) {
     array.push(team[i]
    
     .toUpperCase()
-    .trim());
-  }
+    .trim()[0]);
+  };
+  temp = array.sort();
+  res = temp.join('');
+  //console.log(temp,res);
+  return res;
 };
