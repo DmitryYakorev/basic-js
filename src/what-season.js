d5911d31406a5a89d63d71b72a7dae11363df2e8
urakend@mail.ru <urakend@mail.ru>
2020-03-02 22:36:52 +0300

    feat:done task4

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,27 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+   // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Object/hasOwnProperty
+   if (date.hasOwnProperty('getMonth')) {
+    throw new Error();
+  }
+  if (!(date instanceof Date)) {
+    throw new Error();
+  }
+ 
+
+  let getMonth = date.getMonth() + 1;
+ // console.log(getMonth);
+
+  if ( getMonth > 2 && getMonth < 6) {
+    return 'spring';
+  } else if ( getMonth > 5 && getMonth < 9) {
+    return 'summer';
+  } else if ( getMonth > 8 && getMonth < 12) {
+    return 'fall';
+  } else {
+    return 'winter';
+  }
 };
