9313645a1973ba1e1063d9ee06ecafe00e7380e6
urakend@mail.ru <urakend@mail.ru>
2020-03-03 15:27:27 +0300

    feat:done task5

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,9 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    let object = {};
+    let temp = Math.pow(2, disksNumber) - 1;
+    //https://ru.wikipedia.org/wiki/%D0%A5%D0%B0%D0%BD%D0%BE%D0%B9%D1%81%D0%BA%D0%B0%D1%8F_%D0%B1%D0%B0%D1%88%D0%BD%D1%8F
+    //console.log(temp);
+    object.turns = temp;
+    object.seconds = temp / (turnsSpeed / 3600);
+    return object;
 }
