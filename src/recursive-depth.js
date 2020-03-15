c4f9bf5a37c2f36eb31900fad786310a8253b14e
urakend@mail.ru <urakend@mail.ru>
2020-03-15 19:59:38 +0300

    feat:task7

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,11 +1,14 @@
 module.exports = class DepthCalculator {
     calculateDepth( arr ) {
-        i = 1;
-        function flattenDeep(arr) {
-            
-            return arr.reduce((acc, val) => Array.isArray(val) ? {acc.concat(flattenDeep(val));i++;} : acc.concat(val), []);
-         }
-         flattenDeep(arr);
+let i = 1;
+         if (Array.isArray(arr)) {
+            for (let el of arr) {
+              if (Array.isArray(el)) {
+                i = Math.max(this.calculateDepth(el) + 1,i);
+                //console.log(el);
+              }
+            }
          return i
-    }
+        } else return 0
+    };
 };
