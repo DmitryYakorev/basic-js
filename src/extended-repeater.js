ceb5e033d9e2bc90568f7b570ac10023a8c5598c
urakend@mail.ru <urakend@mail.ru>
2020-03-16 11:51:11 +0300

    feat:task8

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,26 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater( str, options ) {
+    if(str === null) str = 'null';    
+    if(str === undefined) str = '';   
+    //console.log('1',str);
+  str = str.toString();
+    let repeatTimes = (options['repeatTimes'] !== undefined)? options.repeatTimes : 0;
+    let separator = (options['separator'] !== undefined)? options.separator : '+';
+    if (options['addition'] !== undefined) if (options['addition'] !== null) addition = options.addition.toString()
+    else addition = 'null'
+    else addition = '';
+    
+    
+    
+   
+    let additionRepeatTimes = (options['additionRepeatTimes'] != undefined)? options.additionRepeatTimes : 0;
+    let additionSeparator = (options['additionSeparator'] != undefined)? options.additionSeparator : '|';
+    plus = addition;
+  if  (additionRepeatTimes > 0)  plus = Array(additionRepeatTimes).fill(addition).join( additionSeparator); 
+  if (repeatTimes > 0)  temp =  Array(repeatTimes).fill(str+plus).join(separator)
+  else
+  temp = str+plus;
+
+  return temp;
+
 };
   
