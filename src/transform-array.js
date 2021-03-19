77f963b687a61bcc36424710568455e4eb288df3
urakend@mail.ru <urakend@mail.ru>
2021-03-19 23:18:51 +0300

    feat:task9_1

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,63 +1,43 @@
 module.exports = function transform(arr) {
-    if (!Array.isArray(arr)) {
-        throw new Error();
-    };
-    let t1 = [];
-    for (let i = 0; i < arr.length; i++) {
-        switch(arr[i]) {
-            
-            case '--discard-prev': {
-                if (i - 1 >= 0) {
-                    t1.pop();
-                }
-                else {
-                    t1.push(arr[i]);
-                    console.log(arr[i]);
-
-                 };
-             break;
-            }
-            case '--discard-next': {
-                if (i + 1 < arr.length) {
-                    i++;
-                }
-                else {
-                    t1.push(arr[i]);
-
-                 };
-             break;
-            }
-
-        
-            case '--double-prev': {
-                if (i - 1 >= 0) {
-                    t1.push(arr[i - 1]); 
-                };
-                 /*else*/if (i - 1 < 0) {t1.push(arr[i]);
-                    console.log(t1);
+    if (!arr) throw 'undefined or null';
+    if (!Array.isArray(arr)) throw 'not an array';
+    let res = [];
+    for(let i = 0; i < arr.length;i++){
+        if(typeof(arr[i])=='string'){
+            switch(arr[i]){
+                case '--double-next':
+                    if(i < arr.length - 1 && notSpecial(arr[i+1])) {
+                        res.push(arr[i+1]);
+                        
+                    }
+                    break;
+                case '--double-prev':
                     
-                } 
-            break;
-        }
-        case '--double-next': {
-           // console.log('!');
-            if (i + 1 < arr.length) {
-                t1.push(arr[i + 1]);
-            };
-             /*else*/ if (i + 1 >= arr.length) {t1.push(arr[i]);
-                console.log(t1);
-            } 
-            
-
-            
-         break;
-        }
-            default: {
-                t1.push(arr[i]);
+                    if ( i > 0) res.push(arr[i-1]);
+                    break;
+                case '--discard-next':
+                    if(i < arr.length - 1 && notSpecial(arr[i+1])) i++;
+                    break;
+                case '--discard-prev':
+                    if (res.length > 0) res.pop();
+                    
+                    break;
+          
+                default:
+                    res.push(arr[i]);
+                    break;
             }
-
+        }
+        else
+        {
+            res.push(arr[i]);
         }
     }
-   // console.log(t1);
-    return t1;
+    
+    return res;
 };
+
+function notSpecial (el){
+    return (el !== '--discard-next' && el !== '--discard-prev' &&
+            el !== '--double-next' && el !== '--double-prev'); 
+}
