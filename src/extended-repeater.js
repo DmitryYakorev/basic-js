777dbf80e68a758206c8aa130d5555b8ef05f55e
Dmitry Yakorev <urakend@mail.ru>
2022-04-29 13:41:05 +0300

    feat:repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater( str, options ) {
+  let addition,plus,temp;
+  if(str === null) str = 'null';    
+    if(str === undefined) str = '';   
+    //console.log('1',str);
+  str = str.toString(); 
+  //options = options.toString();
+    let repeatTimes = (options['repeatTimes'] !== undefined)? options.repeatTimes : 0;
+    let separator = (options['separator'] !== undefined)? options.separator : '+';
+    if (options['addition'] !== undefined) if (options['addition'] !== null) addition = options.addition.toString()
+    else addition = 'null'
+    else addition = '';
+    
+    
+    
+   
+    let additionRepeatTimes = (options['additionRepeatTimes'] != undefined)? options.additionRepeatTimes : 0;
+    let additionSeparator = (options['additionSeparator'] != undefined)? options.additionSeparator : '|';
+    plus = addition;
+  if  (additionRepeatTimes > 0)  plus = Array(additionRepeatTimes).fill(addition).join( additionSeparator); 
+  if (repeatTimes > 0)  temp =  Array(repeatTimes).fill(String(str) + plus).join(separator)
+  else
+  temp = String(str) + plus;
+
+  return temp;
 }
 
 module.exports = {
